9cd66411903ba01bb447a7c62448bb6c78a0b774
PieroJK <118009731+PieroJK@users.noreply.github.com>
2023-10-03 01:13:28 -0500

    Cargar datos de tabla marca

--- ProyectIntegrador/src/main/java/com/proyect/proyectintegrador/Controller/CtrMarca.java
+++ ProyectIntegrador/src/main/java/com/proyect/proyectintegrador/Controller/CtrMarca.java
@@ -1,7 +1,40 @@
 
 package com.proyect.proyectintegrador.Controller;
 
+import com.proyect.proyectintegrador.Connection.Connect;
+import com.proyect.proyectintegrador.Entitis.Marca;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class CtrMarca {
     
+     String SQL_CONSULTA ="SELECT cod_marca, nombre_marca, estado from marca;";
+    String SQL_INSERTAR = "{CALL InsertarMarca(?,?)}";
+    
+    public List<Marca> cargarMarca() throws SQLException{
+        List <Marca> marcaList = new ArrayList<>() ;
+
+        try(Connection connection = Connect.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL_CONSULTA);
+            ResultSet resultSet = preparedStatement.executeQuery()){
+            
+            while(resultSet.next()){
+                Marca marca = new Marca();
+                marca.setCodmarca(resultSet.getLong("cod_marca"));
+                marca.setNombre(resultSet.getString("nombre_marca"));
+                marca.setEstado(resultSet.getBoolean("estado"));
+                marcaList.add(marca);
+            }
+            
+        }catch(SQLException e){
+            throw e;
+        }
+        
+        return marcaList;
+    }
 }
--- ProyectIntegrador/src/main/java/com/proyect/proyectintegrador/Entitis/Marca.java
+++ ProyectIntegrador/src/main/java/com/proyect/proyectintegrador/Entitis/Marca.java
@@ -4,4 +4,31 @@ package com.proyect.proyectintegrador.Entitis;
 
 public class Marca {
     
+    private Long codmarca;
+    private String nombre;
+    private Boolean estado;
+
+    public Long getCodmarca() {
+        return codmarca;
+    }
+
+    public void setCodmarca(Long codmarca) {
+        this.codmarca = codmarca;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
 }
